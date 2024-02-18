b657acd1d925bce871cec52f61b45d95ecf2de7e
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-02-18 18:01:28 -0300

    ajustando barra de navegação e pesquisa

--- projeto/stocks/quotes/views.py
+++ projeto/stocks/quotes/views.py
@@ -4,13 +4,19 @@ import json
 
 # Create your views here.
 def home(request):
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=")
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error ..."
 
-    return render(request,"home.html",{"api":api})
+    if request.method == 'POST':
+        ticker = request.POST["ticker"]
+        api_request = requests.get(f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token=pk_0920b6af199d4c87acc868419bf91e38")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error ..."
+
+        return render(request,"home.html",{"api":api})
+    else:
+        return render(request,"home.html",{"ticker":"enter a ticker: ","api":" "})
 
 def about(request):
     return render(request,"about.html",{})
